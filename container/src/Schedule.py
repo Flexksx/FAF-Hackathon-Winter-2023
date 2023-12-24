37a0241ff31396a99cfb4dfabb9a6ae38bc3f794
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-12-24 03:41:57 +0200

    delete Teacher.py

---
+++ container/src/Schedule.py
@@ -0,0 +1,1 @@
+from classes.Selector import Selector
--- container/src/classes/Teacher.py
+++
@@ -1,8 +0,0 @@
-class Teacher:
-    def __init__(self, _id:int,_name:str,_subject:int, _type:str, _days:list) -> None:
-        self.id:int= _id
-        self.name:str = _name
-        self.subject:int = _subject
-        self.type:str = _type
-        self.can:dict = _days
-
